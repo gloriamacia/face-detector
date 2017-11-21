d199b9c58939a378090399aaffada4291e36cbd8
gloriamacia <gloriamaciamunoz@gmail.com>
2017-11-21 16:55:54 +0100

    files added

---
+++ face_detector.py
@@ -0,0 +1,21 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('pictures/girl.jpg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('Gray', gray_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.15, minNeighbors=5)
+
+#print(faces)
+#print(type(faces))
+for x, y, w, h in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
+
+cv2.imshow('Face Detector', resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
